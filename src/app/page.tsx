0e32883dbab438007fb02e40a67b146f0400017c
Josana-Kla <josana.0205@gmail.com>
2023-05-18 23:46:06 -0300

    style: added style pattern of tailwind and started home structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <h1 className="w-20 text-lg font-bold uppercase">Hello World</h1>
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      <div></div>
+
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            {' '}
+            {/* para colocar width específico; leading-relaxed: propriedade mais relaxada; é o line-height igual a 1.625, que é ideal para melhor legibilidade de textos corridos */}
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href=""> criar agora!</a>
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
