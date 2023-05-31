754bd11054e0a59435b99ce77a565406cdafae3c
Josana-Kla <josana.0205@gmail.com>
2023-05-31 15:43:00 -0300

    feat: get user token when registered, redirected to home, saved cookies and got user datas

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com'], // precisa falar o domínio que o next precisa pegar sua imagem pois ele não carrega imagens externas
+  },
+}
 
 module.exports = nextConfig
---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,23 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  const redirectURL = new URL('/', request.url) // vai redirecionar o usuário para a rota / (home da minha aplicação), contida nesse request.url
+
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`, // nome do cookie = token. o path, significa que o cookie ficará disponível em toda a aplicação ao usar o /
+    },
+  })
+}
